b222a253e9f18b098d7593e8aac3bbec1aad1db5
nuomizong <nuomizong@qq.com>
2017-08-22 22:47:32 +0100

    completing DKT

---
+++ FEM/clean_latex.bat
@@ -0,0 +1,3 @@
+DEL *.aux *.bbl *.blg *.log *.out *.bak *.sav *.gz *.brf *.lbl
+cd body
+DEL *.aux *.log
--- modeDef/simulation/env.h
+++
@@ -1,7 +0,0 @@
-//this file holds declarations for some option variables
-
-extern const char* scene_file;
-extern int verbose;
-extern bool draw_penetration;
-extern bool draw_animation;
-extern bool draw_constraints;
